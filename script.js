d4c2be17d22880ee2e56a81047f96416311047c3
Agusunx <112596946+Agusunx@users.noreply.github.com>
2025-03-01 04:35:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function showContent(id) {
     var sections = document.querySelectorAll('body > div');
     sections.forEach(function(section) {
-       
+        if (section.id === id) {
+            section.classList.remove('d-none');
+        } else {
+            section.classList.add('d-none');
+        }
+    });
+}
